c98eeab7ed05480de059f1a568ae32c8c2e5285e
Kemran <109353270+remr2005@users.noreply.github.com>
2023-12-20 15:22:10 +0300

    Add files via upload

---
+++ main/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	log.Fatal(http.ListenAndServe(":8001", r))
+}
---
+++ main/types.go
@@ -0,0 +1,14 @@
+package main
+
+type person struct {
+	id    string
+	name  string
+	money int
+}
+
+type product struct {
+	id          string
+	name        string
+	description string
+	cost        int
+}
